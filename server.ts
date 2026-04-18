938c9bf8eb0b37bdae26b7c3691b98878b48a369
mdmasudulparvezsabbir-bit <mdmasudulparvezsabbir@gmail.com>
2026-04-19 03:45:43 +0600

    Add database connection info and password command
    Added database connection details and password update command.

--- server.ts
+++ server.ts
@@ -1,1 +1,9 @@
 mariadb --host serverless-us-central1.sysp0000.db2.skysql.com --port 4039 --user dbpgf17645190 -p --ssl-verify-server-cert
+serverless-us-central1.sysp0000.db2.skysql.com
+Read-write port
+4039
+Username
+dbpgf17645190
+Default password
+ZzNq%MDYQlQqZNLaHBq40ft
+SET PASSWORD FOR 'dbpgf17645190'@'%' = PASSWORD('Parvez@121400##');
